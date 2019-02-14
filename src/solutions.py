5fd04e09b8b6482692156d03da18d29120451124
Marco Chiarandini <marco@imada.sdu.dk>
2019-02-14 19:28:19 +0100

    restyle

--- src/solutions.py
+++ src/solutions.py
@@ -12,12 +12,6 @@ import matplotlib.pyplot as plt
 
 Tour = list  # Tours are implemented as lists of cities
 
-
-def plot_lines(points, style='bo-'):
-    "Plot lines to connect a series of points."
-    plt.plot([p.x for p in points], [p.y for p in points], style)
-    plt.axis('scaled'); plt.axis('off')
-
 def plot_tsp(algorithm, cities):
     "Apply a TSP algorithm to cities, plot the resulting tour, and print information."
     # Find the solution and time how long it takes
@@ -28,13 +22,21 @@ def plot_tsp(algorithm, cities):
     plot_tour(tour); plt.show()
     print("{} city tour with length {:.1f} in {:.3f} secs for {}"
           .format(len(tour), tour_length(tour), t1 - t0, algorithm.__name__))
-    
+
+
 def plot_tour(tour):
     "Plot the cities as circles and the tour as lines between them. Start city is red square."
     start = tour[0]
     plot_lines(list(tour) + [start])
     plot_lines([start], 'rs') # Mark the start city with a red square
     plt.show()
+
+
+def plot_lines(points, style='bo-'):
+    "Plot lines to connect a series of points."
+    plt.plot([p.x for p in points], [p.y for p in points], style)
+    plt.axis('scaled'); plt.axis('off')
+
 
 def valid_tour(tour, cities):
     "Is tour a valid tour for these cities?"
@@ -52,6 +54,6 @@ def first(collection):
 
 
 
-def shortest_tour(tours): 
+def shortest_tour(tours):
     "Choose the tour with the minimum tour length."
     return min(tours, key=tour_length)
